17475e85e08b8430047042a47e1fe22156211e6b
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-28 22:15:08 +0800

    Update Reverse Vowels.cpp
    done

--- Reverse Vowels.cpp
+++ Reverse Vowels.cpp
@@ -8,5 +8,41 @@
 // Explanation:
 // The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
 
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n=s.length();
+        string result;
+        int left=0;
+        int right=n-1;
+
+        string temp="";
+        string temp1="";
+
+        if(n==0) return {};
+        if(n==1) return s;
+        while(left<right){
+            if(s[left]=='a'||s[left]=='A'||s[left]=='e'||s[left]=='E'||s[left]=='i'||s[left]=='I'||s[left]=='o'||s[left]=='O'||s[left]=='u'||s[left]=='U'){
+                temp=s[left];
+            }else{
+                left++;
+            }
+            if(s[right]=='a'||s[right]=='A'||s[right]=='e'||s[right]=='E'||s[right]=='i'||s[right]=='I'||s[right]=='o'||s[right]=='O'||s[right]=='u'||s[right]=='U'){
+                temp1=s[right];
+            }else{
+                right--;
+            }
+            if((temp!="")&&(temp1!="")){
+                swap(s[left],s[right]);
+                left++;
+                right--;
+                temp="";
+                temp1="";
+            }   
+        }
+        return s;
+    }
+};
+
 
  
