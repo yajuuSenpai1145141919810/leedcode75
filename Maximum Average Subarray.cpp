4b2b1e4555004dce90ec873c1dc59f3c430b73c9
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-29 23:55:33 +0800

    Update Maximum Average Subarray.cpp
    休息一下

--- Maximum Average Subarray.cpp
+++ Maximum Average Subarray.cpp
@@ -3,3 +3,24 @@
 
 // Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
 // Any answer with a calculation error less than 10-5 will be accepted.
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n=nums.size();
+        double result=0;
+        double temp=0;
+        if(n==k){
+            double sum=0;
+            for(int x:nums){
+                sum+=x;
+            }
+            return sum/=k;
+        }
+        for(int i=0;i<n-k;i++){
+            temp=nums[i]+nums[i+1]+nums[i+2]+nums[i+3];
+            if(temp>result) result=temp;
+        }  
+        return result/=k;
+    }
+};
