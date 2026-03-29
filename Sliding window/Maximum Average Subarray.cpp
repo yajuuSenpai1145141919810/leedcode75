ed18375d3aa16c7ce5cb590958ff0552a4a78792
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-30 00:13:17 +0800

    Update and rename Maximum Average Subarray.cpp to Sliding window/Maximum Average Subarray.cpp
    第一種寫法(TLE)

--- Sliding window/Maximum Average Subarray.cpp
+++ Sliding window/Maximum Average Subarray.cpp
@@ -4,12 +4,15 @@
 // Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
 // Any answer with a calculation error less than 10-5 will be accepted.
 
+
+// 很糟糕的寫法
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
+
         int n=nums.size();
-        double result=0;
-        double temp=0;
+
+        // 先計算特例
         if(n==k){
             double sum=0;
             for(int x:nums){
@@ -17,10 +20,25 @@ public:
             }
             return sum/=k;
         }
-        for(int i=0;i<n-k;i++){
-            temp=nums[i]+nums[i+1]+nums[i+2]+nums[i+3];
+
+        // 為了解決負數 所以把第一組設給result
+        double temp=0;
+        for(int i=0;i<k;i++){
+            temp+=nums[i];
+        }
+        temp/=k;
+
+
+        double result=temp;
+        for(int i=0;i<=n-k;i++){
+            double temp=0;
+            // 要注意是i+k!!!
+            for(int j=i;j<i+k;j++){
+                temp+=nums[j];
+            }
+            temp/=k;
             if(temp>result) result=temp;
         }  
-        return result/=k;
+        return result;
     }
 };
