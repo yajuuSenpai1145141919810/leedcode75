0901a1d1ee42df95f6c42c12394c0c276185b463
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-31 23:13:01 +0800

    Create Single Number.cpp
    done

---
+++ Bit Manipulation/Single Number.cpp
@@ -0,0 +1,17 @@
+// 136. Single Number
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a "linear runtime" complexity and use only constant extra space.
+
+// 技巧: 做xor就好
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int x:nums){
+            ans^=x;// 做xor
+        }
+        return ans;     
+    }
+};
+
+ 
